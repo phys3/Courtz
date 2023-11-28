59155606fca812e87f960ce84939be79e723bb1f
Damjan Coric <damjancorica92@gmail.com>
2023-11-28 08:54:56 +0100

    add navigation

--- App.tsx
+++ App.tsx
@@ -1,118 +1,93 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaView } from 'react-native';
+import { StatusBar } from 'react-native';
+import { NativeBaseProvider, View, extendTheme } from 'native-base';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import styled from 'styled-components';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const TabLabel = styled.text`
+  font-size: 12px;
+  align-self: center;
+`;
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+const Xy = () => <View />;
+const theme = extendTheme({
+  // Can simply pass default props to change default behaviour of components.
+  components: {
+    Select: {
+      baseStyle: {
+        _disabled: {
+          bg: 'gray.100',
+          borderColor: 'gray.400',
+        },
+      },
+    },
   },
 });
+
+const Tab = createBottomTabNavigator();
+
+function App(): JSX.Element {
+  return (
+    <NavigationContainer>
+      <NativeBaseProvider theme={theme}>
+        <StatusBar
+          backgroundColor={'#fff'}
+          barStyle={'dark-content'}
+          hidden={false}
+        />
+        <Tab.Navigator>
+          <Tab.Screen
+            name="Stack"
+            component={Xy}
+            listeners={({ navigation }) => ({
+              blur: () => navigation.setParams({ screen: undefined }),
+            })}
+            options={{
+              unmountOnBlur: true,
+              headerShown: false,
+              tabBarIcon: ({ focused }) => (
+                <Icon name="plus" size={20} color="black" />
+              ),
+              tabBarLabel: ({ focused }) => (
+                <TabLabel
+                  numberOfLines={1}
+                  adjustsFontSizeToFit
+                  focused={focused}>
+                  {'Blocks'}
+                </TabLabel>
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="zdzdz"
+            component={Xy}
+            listeners={({ navigation }) => ({
+              blur: () => navigation.setParams({ screen: undefined }),
+            })}
+            options={{
+              unmountOnBlur: true,
+              headerShown: false,
+              tabBarIcon: ({ focused }) => (
+                <Icon name="plus" size={20} color="black" />
+              ),
+              tabBarLabel: ({ focused }) => (
+                <TabLabel
+                  numberOfLines={1}
+                  adjustsFontSizeToFit
+                  focused={focused}>
+                  {'Blocks'}
+                </TabLabel>
+              ),
+            }}
+          />
+        </Tab.Navigator>
+        <SafeAreaView />
+      </NativeBaseProvider>
+    </NavigationContainer>
+  );
+}
 
 export default App;
