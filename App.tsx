bf432c58dc24b4f6cd52935e9c005552b30ff206
Damjan Coric <damjancorica92@gmail.com>
2023-12-27 13:24:24 +0100

    Refactor code to remove ApolloClient and update dependencies

--- App.tsx
+++ App.tsx
@@ -2,29 +2,20 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'react-native';
 import AuthNavigator from './src/navigation/AuthNavigator';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { GluestackUIProvider } from '@gluestack-ui/themed';
-import ENV from './env';
-
-const client = new ApolloClient({
-  uri: ENV.API,
-  cache: new InMemoryCache(),
-});
 
 function App(): JSX.Element {
   return (
-    <ApolloProvider client={client}>
-      <GluestackUIProvider>
-        <NavigationContainer>
-          <StatusBar
-            backgroundColor={'#fff'}
-            barStyle={'dark-content'}
-            hidden={false}
-          />
-          <AuthNavigator />
-        </NavigationContainer>
-      </GluestackUIProvider>
-    </ApolloProvider>
+    <GluestackUIProvider>
+      <NavigationContainer>
+        <StatusBar
+          backgroundColor={'#fff'}
+          barStyle={'dark-content'}
+          hidden={false}
+        />
+        <AuthNavigator />
+      </NavigationContainer>
+    </GluestackUIProvider>
   );
 }
 
